71872c10ac39a63978d7e5744162c9c55a5bab01
Simon Andrews <simon@proteo.me.uk>
2022-03-02 08:57:06 +0000

    Basic structure present.

---
+++ cgi-bin/cluster_storage.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+import pam
+import random
+import shutil
+from pathlib import Path
+from json import dumps
+import cgi
+import cgitb
+cgitb.enable()
+
+storage_folder = "/bi/home/andrewss/Storage/"
+sessions_file = "/bi/home/andrewss/cluster_storage/sessions.txt"
+
+def main():
+    form = cgi.FieldStorage()
+
+    if not "action" in form:
+        send_response(False,"No Action")
+        return
+
+    if form["action"].value == "login":
+        process_login(form["email"].value,form["password"].value)
+
+    elif form["action"].value == "validate_session":
+        person = checksession(form["session"].value)
+        send_response(True,person["first_name"]+" "+person["last_name"])
+
+    else:
+        # Everything else needs validation so let's check that first
+        person = checksession(form["session"].value)
+        if person is None:
+            send_response(False,"Unknown session")
+        
+        elif form["action"].value == "list_folders":
+            list_folders(person)
+
+
+
+def send_response(success,message):
+    if success:
+        print("Content-type: text/plain; charset=utf-8\n\nSuccess: "+message, end="")
+    else:
+        print("Content-type: text/plain; charset=utf-8\n\nFail: "+message, end="")
+
+def send_json(data):
+    print("Content-type: text/json; charset=utf-8\n\n"+dumps(data))
+
+    
+def list_folders(person):
+    folders = []
+
+    report_file = Path(storage_folder+f"storage_report_{person}.txt")
+
+    if report_file.is_file():
+        with open(report_file) as infh:
+            for i,line in enumerate(infh):
+                if i==0:
+                    continue
+                
+                sections = line.strip().split("\t")
+                folders.append({
+                    "user": sections[0],
+                    "folder": sections[1],
+                    "bytes": sections[2],
+                    "readable": sections[3],
+                    "count": sections[4],
+                    "modified": sections[5],
+                    "types": sections[6]
+                })
+
+    send_json(folders)
+
+def process_login (username,password):
+
+    # Check the password
+    p = pam.pam()
+    if p.authenticate(username,password):
+        sessioncode = generate_id(20)
+        tempfile = sessions_file+"sessioncode"
+        with open(tempfile,"w") as out:
+            with open(sessions_file) as sf:
+                for line in sf:
+                    if line.startswith(username):
+                        continue
+                    out.write(line)
+            
+            print(f"{username}\t{sessioncode}",file=out)
+        shutil.move(tempfile,sessions_file)
+
+        send_response(True,sessioncode)
+    else:
+        send_response(False,"Incorrect login")
+
+
+def checksession (sessioncode):
+
+    with open(sessions_file) as infh:
+        for line in infh:
+            line = line.strip()
+            username,session = line.split("\t")
+            if session == sessioncode:
+                return username
+
+    return None
+
+
+
+def generate_id(size):
+    """
+    Generic function used for creating IDs
+    """
+    letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    code = ""
+
+    for _ in range(size):
+        code += random.choice(letters)
+
+    return code
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ js/cluster_storage.js
@@ -0,0 +1,145 @@
+const backend = "/cgi-bin/cluster_storage.py"
+var session = ""
+
+$( document ).ready(function() {
+    show_login()
+
+    // Action when they log in
+    $("#login").click(process_login)
+    $("#password").keypress(function(e){
+        if(e.keyCode == 13){
+            process_login();
+        }
+    });
+
+    // Action when they log out
+    $("#logout").click(logout)
+})
+
+
+function show_login() {
+
+    // Check to see if there's a valid session ID we can use
+
+    session = Cookies.get("cluster_storage_session_id")
+    if (session) {
+        // Validate the ID
+        $.ajax(
+            {
+                url: backend,
+                method: "POST",
+                data: {
+                    action: "validate_session",
+                    session: session,
+                },
+                success: function(session_string) {
+                    if (!session_string.startsWith("Success:")) {
+                        session = ""
+                        Cookies.remove("imagetrack_session_id")
+                        $("#logindiv").modal("show")
+                        show_login()
+                        return
+                    }
+                    var realname = session_string.substring(9)
+                    $("#logindiv").modal("hide")
+                    $("#maincontent").show()
+    
+                    // Get their list of projects
+                    update_folders()
+
+                },
+                error: function(message) {
+                    console.log("Existing session didn't validate")
+                    $("#logindiv").modal("show")
+                }
+            }
+        )
+    }
+    else {
+        $("#logindiv").modal("show")
+    }
+}
+
+function logout() {
+    session_id = ""
+    Cookies.remove("imagetrack_session_id")
+    $("#maincontent").hide()
+    $('#projecttable').DataTable().clear()
+    $("#logindiv").modal("show")
+}
+
+
+
+function process_login() {
+    let username = $("#username").val()
+    let password = $("#password").val()
+
+    // Clear the password so they can't do it again
+    $("#password").val("")
+
+    $.ajax(
+        {
+            url: backend,
+            method: "POST",
+            data: {
+                action: "login",
+                username: username,
+                password: password
+            },
+            success: function(session_string) {
+                let sections = session_string.split(" ")
+                if (!session_string.startsWith("Success")) {
+                    $("#loginerror").html("Login Failed")
+                    $("#loginerror").show()
+                    return
+                }
+                $("#loginerror").hide()
+                session = sections[1]
+
+                Cookies.set("cluster_storage_session_id", session, { secure: true })
+                show_login()
+            },
+            error: function(message) {
+                $("#loginerror").html("Login Failed")
+                $("#loginerror").show()
+            }
+        }
+    )
+}
+
+function update_folders(){
+
+    $.ajax(
+        {
+            url: backend,
+            method: "POST",
+            data: {
+                action: "list_folders",
+                session: session
+            },
+            success: function(folders) {
+                $("#projectbody").empty()
+
+                let t = $('#projecttable').DataTable();
+                t.clear()
+
+                for (let f in folders) {
+                    let folder = folders[f]
+                    t.row.add([
+                        project["user"],
+                        project["folder"],
+                        project["bytes"],
+                        project["readable"],
+                        project["count"],
+                        project["modified"],
+                        project["types"]
+                    ]).draw(false)
+                }
+            },
+            error: function(message) {
+                $("#projectbody").clear()
+            }
+        }
+    )
+
+}
